91ad66a16db373a6ce082b6385eea1fea6a804e4
Bernardita Stitic <bastitic@uc.cl>
2022-07-26 23:42:23 -0400

    Section 16

--- 16. Section 16 (polymorphism)/Account_Util.cpp
+++ 16. Section 16 (polymorphism)/Account_Util.cpp
@@ -1,14 +1,15 @@
 /***
 This challenge starts from the official solution to the previous challenge (Section 15). The following files, which are 
-required for this challenge (Section 16), were provided by the instructor: Account.h, Account.cpp, Account_Util.h, Account_Util.cpp,
-Checking_Account.h, Checking_Account.cpp, Savings_Account.h, Savings_Account.cpp, Trust_Account.h, Trust_Account.cpp, main.cpp. Please
-refer to the course website for these files. 
+required for this challenge (Section 16) and should be modified, were provided by the instructor: Account.h, Account.cpp, Account_Util.h, 
+Account_Util.cpp, Checking_Account.h, Checking_Account.cpp, Savings_Account.h, Savings_Account.cpp, Trust_Account.h, Trust_Account.cpp, main.cpp. 
+Please refer to the course website for these files. 
 
 I will only be sharing my modified Account_Util.cpp file and the additional tests I added in my main.cpp file. This is because the 
 modifications I made to the other files are very similar to the official changes made by the instructor. Also, regarding my Account_Util.cpp, 
-despite my solution being very similar to the official one there are some key differences which is why I've decided to share it. Moreover, the 
+despite my solution being very similar to the official one there are some key differences which is why I've decided to share it. Specifically,
+the differences refer to the for loops, the rest is the same (the first std::cout lines of every function + function signatures). Moreover, the 
 two additional files I created for this challenge, which are related to an abstract class which is used as an interface, are also pretty much 
-the same as the official solution (it also includes two extra files). 
+the same as the ones which are part of the official solution (it also includes two extra files). 
 
 Everything I described in the previous paragraph is influenced by the fact that some code was already available in the challenge description.
 Also, the modifications to make were very clearly described by the instructor.  Therefore, please refer to the course website for the 
